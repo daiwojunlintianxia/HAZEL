dda3d8800a82400173084f28a689824af6dc4393
daiwojunlintianxia <lingyueying666@gmail.com>
2023-05-24 23:01:40 +0800

    keyboard and mouse

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -7,6 +7,10 @@
 #include "Hazel/Log.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
+
 // ---Entry Point----------------
 #include "Hazel/EntryPoint.h"
 // ------------------------------
