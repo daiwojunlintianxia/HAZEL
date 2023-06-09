1599319f8b71545398405e19dc91f594cf6d8b51
daiwojunlintianxia <lingyueying666@gmail.com>
2023-06-09 22:30:02 +0800

    my first shader of engine

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -6,6 +6,7 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/LayerStack.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
+#include "Hazel/Renderer/Shader.h"
 
 namespace Hazel {
 
@@ -34,6 +35,7 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
