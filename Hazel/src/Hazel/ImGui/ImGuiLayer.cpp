3fa98158bf5da5a68c031b4233d773e103d1e5b5
daiwojunlintianxia <lingyueying666@gmail.com>
2023-08-09 22:36:00 +0800

    profile timer

--- Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -80,10 +80,4 @@ namespace Hazel {
 			glfwMakeContextCurrent(backup_current_context);
 		}
 	}
-
-	void ImGuiLayer::OnImGuiRender()
-	{
-		static bool show = true;
-		ImGui::ShowDemoWindow(&show);
-	}
 }
--- Hazel/src/Hazel/ImGui/ImGuiLayer.h
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.h
@@ -15,7 +15,6 @@ namespace Hazel {
 
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
-		virtual void OnImGuiRender() override;
 
 		void Begin();
 		void End();
