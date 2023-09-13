1156fd3688da32a4705fa71134395a2f1555a430
daiwojunlintianxia <lingyueying666@gmail.com>
2023-09-14 00:04:19 +0800

    renderer api improvements

--- SandBox/src/Sandbox2D.cpp
+++ SandBox/src/Sandbox2D.cpp
@@ -41,7 +41,7 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 		Hazel::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Hazel::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Hazel::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Hazel::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, glm::radians(50.0f), m_CheckerboardTexture, 10.0, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Hazel::Renderer2D::EndScene();
 	}
 	// TODO: Add these functions - Shader::SetMat4, Shader::SetFloat4
