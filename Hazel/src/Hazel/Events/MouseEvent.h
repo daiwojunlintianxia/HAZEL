c0bc71e9fe6ee426a28a2af4f52e76ab2f7fa116
daiwojunlintianxia <lingyueying666@gmail.com>
2023-11-05 23:12:42 +0800

    Added rotate batching to Renderer2D and remove unuseless inline

--- Hazel/src/Hazel/Events/MouseEvent.h
+++ Hazel/src/Hazel/Events/MouseEvent.h
@@ -11,8 +11,8 @@ namespace Hazel {
 		MouseMovedEvent(float x, float y)
 			: m_MouseX(x), m_MouseY(y) {}
 
-		inline float GetX() const { return m_MouseX; }
-		inline float GetY() const { return m_MouseY; }
+		float GetX() const { return m_MouseX; }
+		float GetY() const { return m_MouseY; }
 
 		std::string ToString() const override
 		{
@@ -33,8 +33,8 @@ namespace Hazel {
 		MouseScrolledEvent(float xOffset, float yOffset)
 			: m_XOffset(xOffset), m_YOffset(yOffset) {}
 
-		inline float GetXOffset() const { return m_XOffset; }
-		inline float GetYOffset() const { return m_YOffset; }
+		float GetXOffset() const { return m_XOffset; }
+		float GetYOffset() const { return m_YOffset; }
 
 		std::string ToString() const override
 		{
@@ -52,7 +52,7 @@ namespace Hazel {
 	class MouseButtonEvent : public Event
 	{
 	public:
-		inline MouseCode GetMouseButton() const { return m_Button; }
+		MouseCode GetMouseButton() const { return m_Button; }
 
 		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
 	protected:
